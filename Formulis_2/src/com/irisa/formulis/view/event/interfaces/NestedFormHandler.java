2525bed4767e01510f15a457d6eea4e7791d6cfa
pmaillot <pierre.maillot@irisa.fr>
2016-09-01 17:31:34 +0200

    Suppression NestedFormEvent

--- Formulis_2/src/com/irisa/formulis/view/event/interfaces/NestedFormHandler.java
+++
@@ -1,9 +0,0 @@
-package com.irisa.formulis.view.event.interfaces;
-
-import com.irisa.formulis.view.event.NestedFormEvent;
-
-public interface NestedFormHandler {
-	
-	public void onNestedForm(NestedFormEvent event);
-	
-}
