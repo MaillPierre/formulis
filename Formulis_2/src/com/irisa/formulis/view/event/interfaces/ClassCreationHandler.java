e60f1198abe6268a9b5c16bf713d8368b4ffc932
pmaillot <pierre.maillot@irisa.fr>
2016-09-28 14:46:53 +0200

    Correction alignement + creation classe

---
+++ Formulis_2/src/com/irisa/formulis/view/event/interfaces/ClassCreationHandler.java
@@ -0,0 +1,9 @@
+package com.irisa.formulis.view.event.interfaces;
+
+import com.irisa.formulis.view.event.ClassCreationEvent;
+
+public interface ClassCreationHandler {
+
+	public void onClassCreation(ClassCreationEvent event);
+	
+}
