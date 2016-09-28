8295b43274b85f5d658cbca1a59f9eb64eab073b
pmaillot <pierre.maillot@irisa.fr>
2016-09-28 14:47:34 +0200

    Creation classe fin

---
+++ Formulis_2/src/com/irisa/formulis/view/event/ClassCreationEvent.java
@@ -0,0 +1,11 @@
+package com.irisa.formulis.view.event;
+
+import com.google.gwt.user.client.ui.Widget;
+
+public class ClassCreationEvent extends FormEvent {
+
+	public ClassCreationEvent(Widget src) {
+		super(src);
+	}
+
+}
